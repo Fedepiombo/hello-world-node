2a433c9dd57386010f19ffa633c1ea47089b47c3
Fede <fede@192.168.1.60>
2026-08-09 15:40:22 +0200

    Rompo la calcolatrice di proposito

--- math.test.js
+++ math.test.js
@@ -7,3 +7,5 @@ test('somma 2 + 2 per dare 4', () => {
 test('sottrae 5 - 3 per dare 2', () => {
     expect(math.diff(5, 3)).toBe(2);
 });
+
+function sum(a, b) { return a - b; } // Errore intenzionale!
